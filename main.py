ca9643e991b08e8d57bade1f6db7bdc5aa2b2f7f
Toby Wigglesworth <44904736+toby1999@users.noreply.github.com>
2020-08-08 12:26:33 +0100

    update dependensices

--- main.py
+++ main.py
@@ -793,6 +793,7 @@ def KPI_total_reviews_callback(period):
 print("Success")
 if __name__ == '__main__':
     app.run_server(debug=True)
+    # app.server(host='0.0.0.0', port=8080, debug=True)
 
 from flask import escape
 
